c92a8e250f182e4d306ec913adfcfc881fd85209
Kevin J Gomez <kjgmez1@gmail.com>
2018-10-10 04:28:17 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,8 +57,10 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |sea,hol|
-    puts sea
+    sea.to_s.capitalize
+    puts "#{sea}:"
     puts hol
+    binding.pry
   end
 end
 
