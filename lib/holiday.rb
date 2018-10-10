93375cd3466af70ec06ccd96d721826c85b4b5cf
Kevin J Gomez <kjgmez1@gmail.com>
2018-10-10 04:36:37 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{x.capitalize}:"
     hol.each do |k,v|
       y = k.to_s.split(" ")
-      puts "#{k.capitalize}: #{v.join}"
+      puts "#{k.capitalize}: #{v.join(", ")}"
     end
   end
 end
